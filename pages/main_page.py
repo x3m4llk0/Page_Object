9eaf3c0445cf22bd42b434d29c9bdd3f34275f8b
YOUR_NAME <YOUR_EMAIL>
2023-11-13 23:18:15 +0300

    Edit "Page Object" function

---
+++ pages/main_page.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
+
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
+
+    def should_be_login_link(self):
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link")
