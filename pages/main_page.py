c09c11c3819a44ffde61c6698fa2ced033560d3d
YOUR_NAME <YOUR_EMAIL>
2023-11-15 21:20:11 +0300

    Added transition between pages and alert processing

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,17 @@
 from selenium.webdriver.common.by import By
-from pages.base_page import BasePage
 from pages.locators import MainPageLocators
+from pages.base_page import BasePage
+from pages.login_page import LoginPage
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+        alert = self.browser.switch_to.alert
+        alert.accept()
+        return LoginPage(browser=self.browser, url=self.browser.current_url)
+
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -6,7 +6,8 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
@@ -16,7 +17,7 @@ def test_guest_should_see_login_link(browser):
     page.should_be_login_link()
 
 
-def test_should_be_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    login_page = LoginPage(browser, link)
-    login_page.should_be_login_url()  # проверка, что текущий url имеет login
+# def test_should_be_login_url(browser):
+#     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+#     login_page = LoginPage(browser, link)
+#     login_page.should_be_login_url()  # проверка, что текущий url имеет login
